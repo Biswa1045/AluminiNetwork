7dee76cc68d3e3a053f22705d4a7d5b063afd90a
Biswajit Sahu <biswajitsahu1045@gmail.com>
2022-12-09 22:02:52 +0530

    Initial commit

---
+++ app/src/main/java/com/biswa1045/alumininetwork/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.biswa1045.alumininetwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
