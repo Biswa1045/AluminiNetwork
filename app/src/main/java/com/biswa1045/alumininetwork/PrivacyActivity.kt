7b038e9a4c9d7a3343ae0fbbbd5a3ca4461afcde
Biswajit Sahu <biswajitsahu1045@gmail.com>
2023-01-07 23:01:13 +0530

    1.3

--- app/src/main/java/com/biswa1045/alumininetwork/PrivacyActivity.kt
+++ app/src/main/java/com/biswa1045/alumininetwork/PrivacyActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 
 class PrivacyActivity : AppCompatActivity() {
-    lateinit var ex:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_privacy)
--- app/src/main/java/com/biswa1045/alumininetwork/ProfileActivity.kt
+++ app/src/main/java/com/biswa1045/alumininetwork/ProfileActivity.kt
@@ -2,9 +2,10 @@ package com.biswa1045.alumininetwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ProfileActivity : AppCompatActivity() {
-    lateinit var ex:TextView
+    lateinit var ex: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
