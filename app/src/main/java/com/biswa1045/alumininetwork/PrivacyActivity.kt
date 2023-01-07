abee3b7861920ab646717ea14e3543554105e4bd
Biswajit Sahu <biswajitsahu1045@gmail.com>
2023-01-07 22:41:44 +0530

    1.1

---
+++ app/src/main/java/com/biswa1045/alumininetwork/PrivacyActivity.kt
@@ -0,0 +1,13 @@
+package com.biswa1045.alumininetwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class PrivacyActivity : AppCompatActivity() {
+    lateinit var ex:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_privacy)
+    }
+}
