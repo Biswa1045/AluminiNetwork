0140657bef1fe7861790ccb5280f8504578d14d1
Lenovo <sameerpatro321@gmail.com>
2023-01-07 22:45:55 +0530

    1.2

--- app/src/main/java/com/biswa1045/alumininetwork/ProfileActivity.kt
+++ app/src/main/java/com/biswa1045/alumininetwork/ProfileActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ProfileActivity : AppCompatActivity() {
+    lateinit var ex:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
     }
 }
