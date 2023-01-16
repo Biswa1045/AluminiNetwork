67875a19b588627718af8c87142269e6b1902ec5
Lenovo <sameerpatro321@gmail.com>
2023-01-16 17:45:25 +0530

    hiiiii sam

---
+++ app/src/main/java/com/biswa1045/alumininetwork/clubs.java
@@ -0,0 +1,14 @@
+package com.biswa1045.alumininetwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class clubs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clubs);
+    }
+}
