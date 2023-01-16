79cb0638ebee7621385f05bd2f7eb76f15727af8
Lenovo <sameerpatro321@gmail.com>
2023-01-16 18:41:39 +0530

    hiiiii sam2

--- app/src/main/java/com/biswa1045/alumininetwork/clubs.java
+++ app/src/main/java/com/biswa1045/alumininetwork/clubs.java
@@ -1,14 +1,24 @@
 package com.biswa1045.alumininetwork;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class clubs extends AppCompatActivity {
 
+    CardView cardView,cardView2,cardView3,cardView4,cardView5,cardView6,cardView7;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clubs);
+        cardView=findViewById(R.id.cardView);
+        cardView2=findViewById(R.id.cardView2);
+        cardView3=findViewById(R.id.cardView3);
+        cardView4=findViewById(R.id.cardView4);
+        cardView5=findViewById(R.id.cardView5);
+        cardView6=findViewById(R.id.cardView6);
+        cardView7=findViewById(R.id.cardView7);
     }
 }
